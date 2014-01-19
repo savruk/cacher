e4a5f05f6880addc7a533d3997b3391c7d98a99a
savruk <yusufkoseoglu287@gmail.com>
2014-01-19 22:12:38 +0100

    Changed redid client

--- redis.go
+++ redis.go
@@ -3,12 +3,12 @@ package cacher
 import (
 	"log"
 
-	"menteslibres.net/gosexy/redis"
+	"github.com/alphazero/Go-Redis"
 )
 
 func NewRedisEngine() *RedisEngine {
-	client := redis.New()
-	err := client.Connect("127.0.0.1", 6379)
+	spec := redis.DefaultSpec().Db(7)
+	client, err := redis.NewSynchClientWithSpec(spec)
 
 	if err != nil {
 		log.Println("failed to create the client", err)
@@ -19,28 +19,26 @@ func NewRedisEngine() *RedisEngine {
 }
 
 type RedisEngine struct {
-	Client *redis.Client
+	Client redis.Client
 }
 
-func (rc *RedisEngine) Get(key string) (*Item, error) {
+func (rc RedisEngine) Get(key string) (*Item, error) {
 
 	value, err := rc.Client.Get(key)
 	if err != nil {
 		return nil, err
 	}
+
 	return &Item{
 		Key:   key,
 		Value: []byte(value),
 	}, nil
 }
 
-func (rc *RedisEngine) Set(key string, value []byte) (err error) {
-	_, err = rc.Client.Set(key, value)
-	return err
+func (rc RedisEngine) Set(key string, value []byte) (err error) {
+	return rc.Client.Set(key, value)
 }
 
-func (rc *RedisEngine) Flush() (err error) {
-	message, err := rc.Client.FlushDB()
-	log.Printf("%s %s", "FLUSHDB", message)
-	return err
+func (rc RedisEngine) Flush() (err error) {
+	return rc.Client.Flushdb()
 }
